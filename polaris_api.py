95d987f69ed679dfe7ad74f6e4a826b1a856b9d7
Andre Torres <andreh.torres@hotmail.com>
2025-03-02 02:58:57 -0300

    wip: polaris on telegram

--- polaris_api.py
+++ polaris_api.py
@@ -66,7 +66,7 @@ print(f"DB_USER: {DB_USER}")
 print(f"DB_PASS: {DB_PASS}")
 print(f"DB_URL: {DB_URL}")
 
-MONGO_URI = f"{DB_URL}/polaris_db?authSource={DB_USER}"
+MONGO_URI = DB_URL
 client = MongoClient(MONGO_URI)
 db = client["polaris_db"]
 collection = db["user_memory"]
